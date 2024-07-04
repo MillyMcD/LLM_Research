11a8757914a8306c013756e0d9a337e004a8986d
MillyMcD <millymcdowellart@gmail.com>
2024-07-04 19:55:22 +0100

    rm password

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 source llm_env/bin/activate
 
 #create notebook
-jupyter notebook --ip 0.0.0.0 --port 9001 --no-browser --allow-root &
+jupyter notebook --ip 0.0.0.0 --port 9001 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''&
