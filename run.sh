9d6962aaf84594a14a20abac7f11b7728534d590
Milly McDowell <millymcdowellart@gmail.com>
2024-08-27 19:03:35 +0100

    update

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#activate the environment
-source llm_env/bin/activate
-
-#create notebook
-jupyter notebook --ip 0.0.0.0 --port 9001 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''&
