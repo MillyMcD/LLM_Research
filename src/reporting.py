9e38bca133df836411c8a203e87d03a6f40e8bf8
Milly McDowell <millymcdowellart@gmail.com>
2024-09-28 23:20:09 +0100

    Update codebase

--- src/reporting.py
+++ src/reporting.py
@@ -42,16 +42,31 @@ def filter_folder_to_dataframe(pth:Path,model_list:list,ids:list):
     for dir in Path(pth).iterdir():
         if not dir.name in ['gemma2','llama3.1']:
             continue
+        print(dir)
         for file in dir.iterdir():
             if file.stem not in model_list:
                 continue
-    
             df = pd.read_csv(file)
             df = df.loc[df.id.isin(ids)]
-            if file.stem not in records.keys():
-                record = {'accuracy': df['consistent'].sum()/len(df)}
-                record['sem_accuracy'] = (df['sem_score']>0.7).sum()/len(df)
+
+            record = {f'{dir.name}_accuracy': df['consistent'].sum()/len(df)}
+            record['sem_accuracy'] = (df['sem_score']>0.7).sum()/len(df)
+            record['model'] = file.stem
+            #record[f'{dir.name}_df'] = df
+            if file.stem in records.keys():
+                records[file.stem].update(record)
+            else:
                 records[file.stem] = record
-            
-            records[file.stem][f'{dir.name}_accuracy'] = df['accuracy'].mean()
-    return records
+
+    nrecords = []
+    acc_columns = None
+    for k,v in records.items():
+        rec = {k1:v1 for k1,v1 in v.items()}
+        avg = [v1 for k1,v1 in v.items() if 'accuracy' in k1]
+        if acc_columns is None:
+            acc_columns = sorted([k1 for k1,v1 in rec.items() if 'accuracy' in k1])
+        rec['average_accuracy'] = np.mean(avg).item()
+        nrecords.append(rec)
+    acc_columns.append('average_accuracy')
+    df = pd.DataFrame(nrecords)
+    return df
