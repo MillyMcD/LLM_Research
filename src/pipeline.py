d37e19e21bdf2f7f4adcac29aac99e8ee2582981
Milly McDowell <millymcdowellart@gmail.com>
2024-09-28 20:16:46 +0100

    Add pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -29,32 +29,75 @@ def system_prompt_nonspecific():
     s = system_prompt_generic()
     s = "You are a chatbot assistant for the Cybersecurity company METCLOUD. You should be friendly but very professional. "
     s += "You should refer to METCLOUD as 'us' and 'we'. "
-    s += "Read the question carefully and assess if the question is the sort of thing a Cyber security professional could be expected to be asked. "
-    s += "If the answer that question is YES, answer to the best of your abilities but explain that your understanding is only approximate. If the "
-    s += "answer is NO, apologise and say that you cannot help, and explain what you _can_ help with i.e METCLOUD + Cyber help. "
-    s += "Remember, do not attempt to embellish METCLOUDs capabilities and do not make us liable to damages. "
+    s += "Read the question carefully and assess if the question relates to the field of Cyber security and cloud services. "
+    s += "If YES, answer to the best of your abilities, but explain your understanding is only approximate for this question. "
+    s += "If NO, apologise and explain that this question is unrelated to your expertise. Do not try to answer the question. "
+    s += "In summary, only attempt to answer Cyber Security questions! Importantly, do NOT embellish METCLOUDs capabilities. "
+    s += "We do not want to be liable for damages!"
     return s
 
+def system_prompt_cyber():
+    s = "You are a chatbot assistant for the Cybersecurity company METCLOUD. You should be friendly but very professional "
+    s += "You should refer to METCLOUD as 'us' and 'we'. "
+    s += "You have generic cyber security expertise, but you only really understand METCLOUD to a high degree of expertise "
+    s += "Answer any cyber related questions, but you should ALWAYS explain that this understanding is only approximate "
+    s += "because you are built for METCLOUD services. Importantly, do NOT embellish METCLOUDs capabilities. "
+    s += "We do not want to be liable for damages! Remember, we are METCLOUD and only have knowledge on how to do METCLOUD things. "
+    s += "We should only offer rough advice for helping with their cyber issues, and should direct people to more appropriate sources wherever possible"
+    return s
+
+def system_prompt_general():
+    s = "You are a chatbot assistant for the Cybersecurity company METCLOUD. You should be friendly but very professional. "
+    s += "You should refer to METCLOUD as 'us' and 'we'. "
+    s += "In this circumstance, the USER has asked a general question that is outside of our knowledge base and is UNRELATED to our expertise. "
+    s += "You should apologise, and explain that we cannot answer this question. Offer to answer a different more appropriate question. "
+    return s
+    
 def is_metcloud_specific(question:str,model):
     s = '# YOUR ROLE\n'
     s += 'You are a specialised text analysis model designed to detect questions specifically related to METCLOUD, a company '
-    s += 'offering cybersecurity services. Your task is to analyse the input text and deetermine if it contains a question about '
-    s += 'METCLOUD or its services\n'
+    s += 'offering cybersecurity services. Your task is to analyse the input text and determine if it contains a question about '
+    s += 'METCLOUD or its services\n\n'
     s += '# INSTRUCTIONS\n'
-    s += '1. Carefully read the text and analyse it\n'
+    s += '1. Carefully read the text\n'
     s += '2. Determine if the text contains a query about METCLOUD or its services\n'
-    s += '3. Respond ONLY with "True" if the text contains a specific question, or "False" if it does not\n\n'
-    s += 'Consider a question METCLOUD-specific if it:\n'
-    s += ' - Directly mentions METCLOUD by name\n'
+    s += '3. Respond ONLY with "True" if the text contains METCLOUD, or "False" if it does not\n\n'
+    s += 'Consider a question METCLOUD-specific if it matches any of the following criteria:\n'
+    s += ' - the word METCLOUD appears in the question\n'
     s += ' - Asks about cybersecurity services\n'
     s += ' - Inquires about account details, pricing or support relating to METCLOUD\n\n'
     s += 'DO NOT RESPOND "True" for:\n'
     s += '- General questions about cyber security \n'
     s += '- Questions about other companies not relating to METCLOUD\n\n'
     s += '# OUTPUT INSTRUCTIONS\n'
-    s += 'Respond with only a single string "True" or "False". No yapping! Do not act as an assistant.'
+    s += 'Respond with only a single string "True" or "False". No yapping! Do not act as an assistant\n'
+    s += '# SPECIAL INSTRUCTIONS\n'
+    s += 'Read the question token by token. If you see "metcloud" or "METCLOUD", then return "True"'
+    
+    response = ollama.generate(model = model,system=s,prompt = f'Does this question mention METCLOUD? : {question}',options = {'temperature':0.0})
+    judgement =  response['response'].lower()
+    return True if 'true' in judgement else False
 
-    response = ollama.generate(model = model,system=s,prompt = f'Is this question METCLOUD specific? : {question}',options = {'temperature':0.0})
+
+def is_cyber(question:str,model):
+    s = '# YOUR ROLE\n'
+    s += 'You are a specialised text analysis model designed to detect questions specifically related to CYBER SECURITY, cyber defence and so forth. '
+    s += 'Your task is to analyse the input text and determine if it contains a question about cyber related topics and activities\n\n'
+    s += '# INSTRUCTIONS\n'
+    s += '1. Carefully read the text\n'
+    s += '2. Determine if the text contains a query about Cyber Security, Cyber defence or related.\n'
+    s += '3. Respond ONLY with "True" if the text does relate to cyber or "False" if it does not\n\n'
+    s += 'Consider a question related to cyber if it matches any of the following criteria:\n'
+    s += ' - Technical networking questions are asked\n'
+    s += ' - Asks about cybersecurity or cyber defence\n'
+    s += ' - Asks about APT, TTPs or similar\n'
+    s += ' - talks about network issues, hacking and so forth\n'
+    s += 'DO NOT RESPOND "True" for:\n'
+    s += '- General questions about unrelated topics such as pop culture'
+    s += '# OUTPUT INSTRUCTIONS\n'
+    s += 'Respond with only a single string "True" or "False". No yapping! Do not act as an assistant\n'
+    
+    response = ollama.generate(model = model,system=s,prompt = f'Does this question relate to cyber or cyber security? : {question}',options = {'temperature':0.0})
     judgement =  response['response'].lower()
     return True if 'true' in judgement else False
 
@@ -80,17 +123,25 @@ class Pipeline:
                 print('METCLOUD Specific question asked, outside of our context!')
                 response = ollama.generate(model = self.llm_model, system = system_prompt_handoff(), 
                                            prompt = 'Use the system message instructions to respond')
-                return response['response']
+                return response['response'], 'metcloud_specific'
             else:
-                print('Generic question asked, outside of our context!')
-                response = ollama.generate(model = self.llm_model, system = system_prompt_nonspecific(), 
-                                           prompt = question)
-                return response['response']
+                if is_cyber(question,model = self.llm_model):
+                    print('Cyber question!')
+                    response = ollama.generate(model = self.llm_model,
+                                               system = system_prompt_cyber(),
+                                               prompt = question)
+                    return response['response'], 'generic_cyber'
+                else:
+                    print('Generic Question!')
+                    response = ollama.generate(model = self.llm_model,
+                                               system = system_prompt_general(),
+                                               prompt = f'use the system message instructions to explain why you cannot answer this question: {question}')
+                    return response['response'], 'generic_external'
         else:
             print('Info retrieved!')
             user_prompt = question + '\n' + context
             response = ollama.generate(model = self.llm_model, system = system_prompt_with_context(), 
                                        prompt = user_prompt)
-            return response['response']
+            return response['response'], 'retrieval'
         
 
--- src/reporting.py
+++ src/reporting.py
@@ -17,7 +17,8 @@ def folder_to_dataframe(pth:Path,model_list:list):
                 records[file.stem] = {f'{dir.name}_accuracy':df['accuracy'].mean(),
                           'model':file.stem,
                           'time':df['mean_time'].mean(),
-                          'tps':df['mean_tps'].mean()}
+                          'tps':df['mean_tps'].mean(),
+                          'sem_accuracy':df['sem_acc'].mean()}
             
             records[file.stem][f'{dir.name}_accuracy'] = df['accuracy'].mean()
 
@@ -27,10 +28,30 @@ def folder_to_dataframe(pth:Path,model_list:list):
         rec = {k1:v1 for k1,v1 in v.items()}
         avg = [v1 for k1,v1 in v.items() if 'accuracy' in k1]
         if acc_columns is None:
-            acc_columns = [k1 for k1,v1 in rec.items() if 'accuracy' in k1]
+            acc_columns = sorted([k1 for k1,v1 in rec.items() if 'accuracy' in k1])
         rec['average_accuracy'] = np.mean(avg).item()
         nrecords.append(rec)
     acc_columns.append('average_accuracy')
     df = pd.DataFrame(nrecords)
     df = df[['model','time','tps',*acc_columns]]
     return df
+
+
+def filter_folder_to_dataframe(pth:Path,model_list:list,ids:list):
+    records = {}
+    for dir in Path(pth).iterdir():
+        if not dir.name in ['gemma2','llama3.1']:
+            continue
+        for file in dir.iterdir():
+            if file.stem not in model_list:
+                continue
+    
+            df = pd.read_csv(file)
+            df = df.loc[df.id.isin(ids)]
+            if file.stem not in records.keys():
+                record = {'accuracy': df['consistent'].sum()/len(df)}
+                record['sem_accuracy'] = (df['sem_score']>0.7).sum()/len(df)
+                records[file.stem] = record
+            
+            records[file.stem][f'{dir.name}_accuracy'] = df['accuracy'].mean()
+    return records
